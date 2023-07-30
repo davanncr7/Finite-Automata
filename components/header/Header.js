772e6a0df310d7fb7a8aacb37cda82152d9b07f6
Davann CR <davanncr@gmail.com>
2023-07-30 17:32:55 +0700

    final_update

--- components/header/Header.js
+++ components/header/Header.js
@@ -6,6 +6,9 @@ export default function Header(){
     return <>
     <div className="bg-pink-900 w-full h-14 flex justify-between px-7  overflow-x-hidden fixed top-0 left-0 z-30">
         <MenuBar/>
+        <div className="flex items-center justify-center text-white font-semibold">
+            <a href="mailto:davanncr@gmail.com" className="underline">Feadback</a>
+        </div>
     </div>
     </>
 }
